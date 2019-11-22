2e67a14361d25cb25eae7c233c47615e684ffa61
Jeremy Kim <thk116@gmail.com>
2019-11-21 17:43:12 -0800

    elements outlined with some styling, functional inputs with values retrieved upon button click

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 // Write your JavaScript here
+var boomBtn = document.getElementById('calcBtn')
+console.log(boomBtn)
+
+boomBtn.addEventListener('click', () => {
+    var due = document.getElementById('due').value
+    var received = document.getElementById('received').value
+    console.log(`due: ${due}  received: ${received}`)
+})
