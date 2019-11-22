2f58056cfb0f89b4391bb163ef2accb03dcebf21
Jeremy Kim <thk116@gmail.com>
2019-11-22 15:15:35 -0800

    working on calculating change

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 // Write your JavaScript here
-var boomBtn = document.getElementById('calcBtn')
-console.log(boomBtn)
+var boomBtn = document.getElementById('calcBtn');
+console.log(boomBtn);
 
 boomBtn.addEventListener('click', () => {
-    var due = document.getElementById('due').value
-    var received = document.getElementById('received').value
-    console.log(`due: ${due}  received: ${received}`)
-})
+    handleBoomBtn();
+});
+
+function handleBoomBtn() {
+    var due = document.getElementById('due').value;
+    var received = document.getElementById('received').value;
+    console.log(`due: ${due}  received: ${received}`);
+    var change = calculateChange(due, received);
+    console.log(`change: ${change}`);
+};
+
+function calculateChange(d, r) {
+    var change = r - d;
+    var roundDownChange = change%1
+    var cents = Number(roundDownChange.toFixed(2))
+ 
+    
+    console.log(`change: ${change} roundDownChange: ${roundDownChange} math: ${cents}`)
+
+};
